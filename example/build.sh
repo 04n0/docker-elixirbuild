78f7ab6d12eb5c82751f8349c0d74100f9c90b19
04n0 <04n0@604.sk>
2017-04-16 10:42:45 +0200

    fix - use cd ... || exit in case cd fails (SC2164)

--- example/build.sh
+++ example/build.sh
@@ -16,23 +16,23 @@ echo "---"
 # install hex package manager and rebar dependency tool
 mix local.hex --force && mix local.rebar --force
 #
-cd ${MYAPP_BUILD_DIR}
+cd ${MYAPP_BUILD_DIR} || exit
 # get hex package depndencies
 mix deps.get
 mix deps.compile
 # install nodejs packages
-cd ${MYAPP_BUILD_DIR}/apps/myapp_web/
+cd ${MYAPP_BUILD_DIR}/apps/myapp_web/ || exit
 npm install
 # install bower packages
 bower install --allow-root
 # brunch - build a production folder
 ./node_modules/brunch/bin/brunch b -p
-cd ${MYAPP_BUILD_DIR}
+cd ${MYAPP_BUILD_DIR} || exit
 # compile applications
 mix compile --env=${MIX_ENV} --verbose=verbose
 # create phoenix digest for static and create a upgradable release
-cd ${MYAPP_BUILD_DIR}/apps/myapp_web/
+cd ${MYAPP_BUILD_DIR}/apps/myapp_web/ || exit
 mix do phoenix.digest --env=${MIX_ENV} --verbose=verbose
-cd ${MYAPP_BUILD_DIR}
+cd ${MYAPP_BUILD_DIR} || exit
 mix release --env=${MIX_ENV} --verbose=verbose --upgrade
-cd /
+cd / || exit
